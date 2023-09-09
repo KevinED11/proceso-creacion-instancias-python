f3c298814dac21a7fcff8c23a59c563a6063816e
KevinED11 <mendez_kael@hotmail.com>
2023-09-09 16:33:52 -0700

    2

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from typing import Self
-class Persona:
-    def __new__(cls, *args, **kwargs) -> object:
+import logging
+
+
+logging.basicConfig(
+    filename="main.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+class Person(object):
+    """
+    Creates a new instance of the Persona class.
+    """
+
+    def __new__(cls, *args, **kwargs) -> "Person":
+        logging.info("Creando una nueva persona.")
+
         print("Nueva instancia de persona creada.")
         return super().__new__(cls)
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int, surname: str) -> None:
         self.name = name
         self.age = age
+        self.surname = surname
 
     def __repr__(self) -> str:
         name = self.name
@@ -14,5 +31,26 @@ class Persona:
         return f"{type(self).__name__}({name=}, {age=})"
 
 
-kevin = Persona(name="kevin", age=22)
-print(kevin)
+Person.__name__ = "NewPerson"
+print(Person.__name__)
+
+
+persons: list[Person] = [
+    Person("kevin", 22, "KevinED11"),
+    Person("alberto", 22, "AlbertoED11"),
+]
+
+
+def find_person(surname: str) -> Person:
+    """
+    Find a person with the given surname.
+    """
+    return list(filter(lambda p: p.surname == surname, persons))[0]
+
+
+def main() -> None:
+    print(find_person(surname="KevinED11"))
+
+
+if __name__ == "__main__":
+    main()
